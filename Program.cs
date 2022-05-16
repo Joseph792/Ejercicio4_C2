edd965a19ac622e3c46b36e6342e84895ca6ffb4
José792 <joseph_camilo@ucne.edu.do>
2022-05-15 20:21:43 -0400

    Agregando el proyecto

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+            double c, f;
+
+            Console.Write("Ingrese la cantidad en celsius: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            
+            f = (c * 9 / 5) + 32;
+
+            Console.WriteLine("Grados Fahrenhait es igual a: " + f);
